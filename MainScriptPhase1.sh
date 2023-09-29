868b2770b1318e5d5d6793a83fd0328110bd004c
Saleh Govahi <115073548+SalehGovahi@users.noreply.github.com>
2023-09-29 10:28:58 +0330

    This is a script to clone phase 2

--- MainScriptPhase1.sh
+++ MainScriptPhase1.sh
@@ -13,44 +13,7 @@ function _makeLogTitle(){
     echo "" >> error.logs
 }
 
-
-if [[ $EUID -eq 0 ]]; then
-
-    ping -c 1 8.8.8.8 >/dev/null 2>&1
-
-    if [[ $? -eq 0 ]]; then
-        
-        if ! command -v git &> /dev/null; then
-            
-            echo "Git is not installed on your system. To continue running this script, you should install Git on your system."
-            read -p "Do you want to install it? [Y/n] " response
-            response=${response,,}
-
-            # If the user wants to install git, install it
-            if [[ $response =~ ^(y| ) ]] || [[ -z $response ]]; then
-                
-                sudo apt-get -y install git > /dev/null 2>&1
-
-            else
-                echo "Skipping from this step is not allowed."
-                echo "You should have Git to continue running script."
-                exit 1
-            fi
-        fi
-        
-        sudo rm -r EmperorPenguinProject > /dev/null 2>&1
-        echo "Cloning Project ..."
-        _makeLogTitle "Cloning project" 
-        git clone https://github.com/SalehGovahi/EmperorPenguinProject.git --branch Phase1
-        cd EmperorPenguinProject
-        sudo bash Phase1.sh
-
-    else
-        echo "You should connect to the internet to run this script. Try again later."
-    fi
-else
-    echo "This script must be run as root."
-    exit 1
-fi
-
-
+_makeLogTitle "Cloning project"
+git clone -b Phase1 https://github.com/SalehGovahi/EmperorPenguinProject.git 2>> error.logs >/dev/null
+cd EmperorPenguinProject
+sudo bash Phase1.sh
