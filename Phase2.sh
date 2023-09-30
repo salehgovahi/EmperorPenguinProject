5b5f45b9d0c7edd1c6e84d4bb0fa6ff96e91be2e
Saleh Govahi <115073548+SalehGovahi@users.noreply.github.com>
2023-09-30 10:04:41 +0330

    Update Phase2.sh

--- Phase2.sh
+++ Phase2.sh
@@ -282,7 +282,7 @@ function _runningImgProxy() {
     if [[ $response =~ ^(y| ) ]] || [[ -z $response ]]; then
         
         cd /opt/project
-        sudo CGO_LDFLAGS_ALLOW="-s|-w" go build -buildvcs=false -o /usr/local/bin/imgproxy
+	CGO_LDFLAGS_ALLOW="-s|-w" go build -buildvcs=false -o /usr/local/bin/imgproxy
 
         
         if [ -f /usr/local/bin/imgproxy ] && [ $? -eq 0 ]; then
